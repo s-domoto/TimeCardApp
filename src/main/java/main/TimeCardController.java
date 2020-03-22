093ee3c9d06a87d2cdeebb126a57310264ab64d0
domoto <koremori0315@yahoo.co.jp>
2020-03-22 11:24:25 +0900

    CSV入出力機能追加

--- src/main/java/main/TimeCardController.java
+++ src/main/java/main/TimeCardController.java
@@ -1,10 +1,20 @@
 package main;
- 
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.TimeZone;
 
@@ -19,12 +29,12 @@ import org.springframework.ui.Model;
 
 @Controller
 public class TimeCardController {
-	
+
 	@Autowired
 	TimeCardRepository timeCardRepo;
 	@Autowired
 	UserInfoRepository userInfoRepo;
-	
+
 	List<UserInfoEntity> userData;
 	List<WorkingTimeEntity> workingTimeList;
 
@@ -33,15 +43,15 @@ public class TimeCardController {
 	String getIndex() {
 		return "index";
 	}
-	
+
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	String getMain(Model model) {
 		Collections.reverse(workingTimeList);
-		
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        //Principalからログインユーザの情報を取得
-        String userId = auth.getName();
-		
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		//Principalからログインユーザの情報を取得
+		String userId = auth.getName();
+
 		if(workingTimeList.get(0).getInTime().isEmpty()) {
 			model.addAttribute("inTimeText", "IN");
 		}else {
@@ -52,14 +62,14 @@ public class TimeCardController {
 		}else {
 			model.addAttribute("outTimeText", workingTimeList.get(0).getOutTime());
 		}
-        model.addAttribute("userId", userId);
+		model.addAttribute("userId", userId);
 		model.addAttribute("workingTimeTable", "勤務表");
 		return "main";
 	}
 
 	@RequestMapping(value = "/workingTimeTable", method = RequestMethod.GET)
 	String getWorkingTimeTable() {
-		
+
 		return "workingTimeTable";
 	}
 
@@ -67,67 +77,67 @@ public class TimeCardController {
 	String postIndexForm(Model model) {		
 
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        //Principalからログインユーザの情報を取得
-        String userId = auth.getName();
-        
-        try {
-        	workingTimeList = timeCardRepo.findWorkingTime(userId);
-        }catch(Exception e) {
-        	e.printStackTrace();
-        }
-			
-        // 出社打刻・退社打刻ともにされていない場合は「IN」、「OUT」を表示
-        if (!(workingTimeList.get(0).getInTime().isEmpty()) && !(workingTimeList.get(0).getOutTime().isEmpty())) {
-        	model.addAttribute("userId", userId);
-        	model.addAttribute("inTimeText", "IN");	
-        	model.addAttribute("outTimeText", "OUT");
-        	model.addAttribute("workingTimeTable", "勤務表");
-        	return "main";
-        }
+		//Principalからログインユーザの情報を取得
+		String userId = auth.getName();
 
-        // 出社打刻がされていない場合は「IN」、されている場合は出社時間を表示
-        if (workingTimeList.get(0).getInTime().isEmpty()) {
-        	model.addAttribute("inTimeText", "IN");	
-        }else {
-        	model.addAttribute("inTimeText", workingTimeList.get(0).getInTime());				
-        }
-        //　退社打刻がされていない場合は「OUT」、されている場合は退社時間を表示
-        if(workingTimeList.get(0).getOutTime().isEmpty()) {
-        	model.addAttribute("outTimeText", "OUT");
-        }else {
-        	model.addAttribute("outTimeText", workingTimeList.get(0).getOutTime());
-        }
-        model.addAttribute("userId", userId);
-        model.addAttribute("workingTimeTable", "勤務表");
-        return "main";
+		try {
+			workingTimeList = timeCardRepo.findWorkingTime(userId);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		// 出社打刻・退社打刻ともにされていない場合は「IN」、「OUT」を表示
+		if (!(workingTimeList.get(0).getInTime().isEmpty()) && !(workingTimeList.get(0).getOutTime().isEmpty())) {
+			model.addAttribute("userId", userId);
+			model.addAttribute("inTimeText", "IN");	
+			model.addAttribute("outTimeText", "OUT");
+			model.addAttribute("workingTimeTable", "勤務表");
+			return "main";
+		}
+
+		// 出社打刻がされていない場合は「IN」、されている場合は出社時間を表示
+		if (workingTimeList.get(0).getInTime().isEmpty()) {
+			model.addAttribute("inTimeText", "IN");	
+		}else {
+			model.addAttribute("inTimeText", workingTimeList.get(0).getInTime());				
+		}
+		//　退社打刻がされていない場合は「OUT」、されている場合は退社時間を表示
+		if(workingTimeList.get(0).getOutTime().isEmpty()) {
+			model.addAttribute("outTimeText", "OUT");
+		}else {
+			model.addAttribute("outTimeText", workingTimeList.get(0).getOutTime());
+		}
+		model.addAttribute("userId", userId);
+		model.addAttribute("workingTimeTable", "勤務表");
+		return "main";
 	}
-//	}
+	//	}
 
 	@RequestMapping(value = "/inTimeForm", method = RequestMethod.GET)
 	public String getInTimeForm(
-		@RequestParam(value="inTime", required = false)
-		Model model) {
-//		model.addAttribute("inTimeText", "IN");
+			@RequestParam(value="inTime", required = false)
+			Model model) {
+		//		model.addAttribute("inTimeText", "IN");
 		return "main";
 	}
 
 	@RequestMapping(value = "/inTimeForm", method = RequestMethod.POST)
 	public String inTimeInsert( Model model, @RequestParam("inTime") String inTime) {
-		
-        Calendar cl = Calendar.getInstance();
+
+		Calendar cl = Calendar.getInstance();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		
+
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        //Principalからログインユーザの情報を取得
-        String userId = auth.getName();
-		
-        try {
-        	workingTimeList = timeCardRepo.findWorkingTime(workingTimeList.get(0).getUserId());
-        }catch(Exception e) {
-        	e.printStackTrace();
-        }
-        		
-		
+		//Principalからログインユーザの情報を取得
+		String userId = auth.getName();
+
+		try {
+			workingTimeList = timeCardRepo.findWorkingTime(workingTimeList.get(0).getUserId());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+
 		// 本日かどうか判定
 		if(workingTimeList.get(0).getDate().equals(sdf.format(cl.getTime()))) {
 
@@ -139,14 +149,14 @@ public class TimeCardController {
 				model.addAttribute("workingTimeTable", "勤務表");
 				return "main";
 
-			// 出社時間が空でない、かつ退社時間が空でない
+				// 出社時間が空でない、かつ退社時間が空でない
 			}else if(!(workingTimeList.get(0).getInTime().isEmpty()) && !(workingTimeList.get(0).getOutTime().isEmpty())) {
 				model.addAttribute("userId", userId);
 				model.addAttribute("inTimeText", workingTimeList.get(0).getInTime());
 				model.addAttribute("outTimeText", workingTimeList.get(0).getOutTime());
 				model.addAttribute("workingTimeTable", "勤務表");
 				return "main";
-				
+
 			}else {
 				model.addAttribute("userId", userId);
 				model.addAttribute("inTimeText", "IN");
@@ -154,41 +164,41 @@ public class TimeCardController {
 				model.addAttribute("workingTimeTable", "勤務表");
 				return "main";
 			}
-			
+
 		}else {
 			// 日付が変わっていたら打刻する
 			// 出社打刻・退社打刻ともにされていない場合は出社打刻する
-//			if(!(workingTimeList.get(0).getInTime().isEmpty()) && !(workingTimeList.get(0).getOutTime().isEmpty())) {
-			
+			//			if(!(workingTimeList.get(0).getInTime().isEmpty()) && !(workingTimeList.get(0).getOutTime().isEmpty())) {
+
 			// 日付と時間に分割
 			String[] inTimeArray = inTime.split(",", 0);		
 
 			// 年、月、日に分割
 			String str = inTimeArray[0].replace("-", ",");	
 			String[] dateArray = str.split(",", 0);
-			
-//			int intWeekDay;
+
+			//			int intWeekDay;
 			String strWeekDay = "";
-			
+
 			// 曜日を取得
-//			intWeekDay = cl.get(Calendar.DAY_OF_WEEK);
+			//			intWeekDay = cl.get(Calendar.DAY_OF_WEEK);
 			switch (cl.get(Calendar.DAY_OF_WEEK)) {
-		        case Calendar.SUNDAY: strWeekDay = "日"; break;
-		        case Calendar.MONDAY: strWeekDay = "月"; break;
-		        case Calendar.TUESDAY: strWeekDay = "火"; break;
-		        case Calendar.WEDNESDAY: strWeekDay = "水"; break;
-		        case Calendar.THURSDAY: strWeekDay = "木" ; break;
-		        case Calendar.FRIDAY: strWeekDay = "金"; break;
-		        case Calendar.SATURDAY: strWeekDay = "土"; break;
-		    }
-			
+			case Calendar.SUNDAY: strWeekDay = "日"; break;
+			case Calendar.MONDAY: strWeekDay = "月"; break;
+			case Calendar.TUESDAY: strWeekDay = "火"; break;
+			case Calendar.WEDNESDAY: strWeekDay = "水"; break;
+			case Calendar.THURSDAY: strWeekDay = "木" ; break;
+			case Calendar.FRIDAY: strWeekDay = "金"; break;
+			case Calendar.SATURDAY: strWeekDay = "土"; break;
+			}
+
 			// 時刻を15分区切りに変換
 			String[] timeArray = new String[3];
 			timeArray = inTimeArray[1].split(":",0);
 			int min = Integer.parseInt(timeArray[1]);
 			int pause = 15;
 			int workTime = (min / pause) * pause;
-			
+
 			if(inTimeArray[1].compareTo( "9:00:00") != 1) {
 				timeArray[0] = "9";
 				timeArray[1] = "00";
@@ -197,10 +207,10 @@ public class TimeCardController {
 				timeArray[1] = String.valueOf(workTime);
 				timeArray[2] = "00";
 			}
-			
+
 			// 分割していた時刻を基の形に戻す
 			String time = timeArray[0] + ":" + timeArray[1] + ":" + timeArray[2];
-			
+
 			if(!(inTime == "")) {
 				try {
 					timeCardRepo.insertWorkingTime(workingTimeList.get(0).getUserId(),
@@ -236,76 +246,74 @@ public class TimeCardController {
 				model.addAttribute("workingTimeTable", "勤務表");
 				return "main";
 			}
-//			}
-			
+			//			}
+
 		}
 	}
 
 	@RequestMapping(value = "/outTimeForm", method = RequestMethod.GET)
 	public String getOutTimeForm(
-		@RequestParam(value="outTime", required = false)
-		Model model) {
+			@RequestParam(value="outTime", required = false)
+			Model model) {
 		return "main";
 	}
 
 	@RequestMapping(value = "/outTimeForm", method = RequestMethod.POST)
 	public String outTimeUpdate( Model model, @RequestParam("outTime") String outTime) {
 
-        Calendar cl = Calendar.getInstance();
+		Calendar cl = Calendar.getInstance();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		
+
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        //Principalからログインユーザの情報を取得
-        String userId = auth.getName();
-        
-        try {
-        	workingTimeList = timeCardRepo.findWorkingTime(workingTimeList.get(0).getUserId());
-        }catch(Exception e) {
-        	e.printStackTrace();
-        }
-		
+		//Principalからログインユーザの情報を取得
+		String userId = auth.getName();
+
+		try {
+			workingTimeList = timeCardRepo.findWorkingTime(workingTimeList.get(0).getUserId());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
 		// 本日かどうか判定
 		if(workingTimeList.get(0).getDate().toString().contentEquals(sdf.format(cl.getTime()).toString())) {
 
 			// 出社打刻がされている場合は退勤打刻を行う
 			if(!(workingTimeList.get(0).getInTime().isEmpty()) && workingTimeList.get(0).getOutTime().isEmpty()) {
-				
+
 				// 日付と時間に分割
 				String[] outTimeArray = outTime.split(",", 0);
-				
+
 				// タイムカードNoを文字列に変換
 				String timeCardNo = String.valueOf((workingTimeList.get(0).getTimeCardNo()));
-				
+
 				// 年、月、日に分割
 				String str = outTimeArray[0].replace("-", ",");	
 				String[] dateArray = str.split(",", 0);
-								
+
 				// 時刻を15分区切りに変換
 				String[] timeArray = new String[3];
 				timeArray = outTimeArray[1].split(":",0);
 				int min = Integer.parseInt(timeArray[1]);
 				int pause = 15;
 				int workTime = (min / pause) * pause;
-			
+
 				timeArray[1] = String.valueOf(workTime);
 				timeArray[2] = "00";
-				
+
 				// 分の単位を合わせる
 				if(timeArray[1].equals("0")) {
 					timeArray[1] = "00";
 				}
-				
+
 				// 休憩時間
 				String breakTime = "";
 				// 残業時間
 				String overTime = "";
-				
-				
+
+
 				// 退勤時間(15分区切り)
 				String strEndTime = timeArray[0] + ":" + timeArray[1] + ":" + timeArray[2];		
 
-//				strEndTime = outTimeArray[0] + " " + strEndTime;
-				
 				// 出勤時間(15分区切り)
 				String strInTime = workingTimeList.get(0).getWorkStartTime();
 
@@ -317,7 +325,7 @@ public class TimeCardController {
 				int diffTime = endTime - startTime;
 				int overHour = diffTime / 60;
 				int overMin = diffTime % 60;
-						
+
 				if(overHour >= 8) {
 					breakTime = "1:00";
 
@@ -332,41 +340,6 @@ public class TimeCardController {
 					breakTime = "0:00";
 					overTime = "0:00";
 				}
-				// 残業時間を
-//				SimpleDateFormat formatter = new SimpleDateFormat("yy-MM-dd HH:mm:ss");
-//				Date startDateTime = new Date();
-//				Date endDateTime = new Date();
-//				
-//				
-//				try {
-//					startDateTime = formatter.parse(strInTime);
-//					endDateTime = formatter.parse(workEndTime);
-//				} catch (ParseException e1) {
-//					// TODO Auto-generated catch block
-//					e1.printStackTrace();
-//				}
-//				long diffTime = endDateTime.getTime() - startDateTime.getTime();
-//				SimpleDateFormat timeFormatter = new SimpleDateFormat ("HH:mm:ss Z");
-//				timeFormatter.setTimeZone(TimeZone.getTimeZone("GMT"));
-//				
-//				// 8時間以上の勤務時間
-//				String strDiffTime = timeFormatter.format(new Date(diffTime - 32400000));
-				
-				
-//				if(diffTime >= 32400000) {
-//					breakTime = "1:00";
-//					overTime = strDiffTime;
-//				}else {
-//					breakTime = "0:00";
-//					overTime = "0:00";
-//				}
-//				// 休憩時間を算出
-//				// 時間のみ取り出して数値に変換
-//				int intWorkStartTime = Integer.parseInt((workingTimeList.get(0).getWorkStartTime()).substring(0, 1));
-//				int intOutTime = Integer.parseInt((strOutTime));
-//				int resultTime = intOutTime - intWorkStartTime;
-//				
-
 
 				try {
 					timeCardRepo.updateOutTime(timeCardNo, breakTime, overTime, outTimeArray[1], strEndTime);
@@ -422,37 +395,179 @@ public class TimeCardController {
 
 	@RequestMapping(value = "/workingTimeTable", method = RequestMethod.POST)
 	public String dispWorkingTimeTable(Model model) {
-		
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        //Principalからログインユーザの情報を取得
-        String userId = auth.getName();
-		
-        model.addAttribute("userId", userId);
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		//Principalからログインユーザの情報を取得
+		String userId = auth.getName();
+
+		model.addAttribute("userId", userId);
 		return "workingTimeTable";
 	}
-	
+
 	@RequestMapping(value = "/dateForm", method = RequestMethod.GET)
 	public String getDate(Model model, @RequestParam("year") String year, @RequestParam("month") String month) {
 
 		// SQL検索用に文字列を調整
 		String date = year + "-" + month + "%";
-		
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        //Principalからログインユーザの情報を取得
-        String userId = auth.getName();
-        
-        try {
-        // ユーザーIDと取得年月から対象データを取得
-        	workingTimeList = timeCardRepo.findWorkingTime(userId, date);
-        }catch(Exception e) {
-        	e.printStackTrace();
-        }
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		//Principalからログインユーザの情報を取得
+		String userId = auth.getName();
+
+		try {
+			// ユーザーIDと取得年月から対象データを取得
+			workingTimeList = timeCardRepo.findWorkingTime(userId, date);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 
 		model.addAttribute("userId", userId);
 		model.addAttribute("year", workingTimeList.get(0).getYear() + "年");
 		model.addAttribute("month", workingTimeList.get(0).getMonth() + "月");
-        model.addAttribute("workingTimeList", workingTimeList);
+		model.addAttribute("workingTimeList", workingTimeList);
 		return "workingTimeTable";
 	}
 
+	@RequestMapping(value = "/csvOutForm", method = RequestMethod.POST)
+	public String getOutCsv(Model model) {
+
+		// SQL検索用に文字列を調整
+		String date = workingTimeList.get(0).getYear() + "-" + workingTimeList.get(0).getMonth() + "%";
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		//Principalからログインユーザの情報を取得
+		String userId = auth.getName();
+
+		try {
+			// ユーザーIDと取得年月から対象データを取得
+			workingTimeList = timeCardRepo.findWorkingTime(userId, date);
+
+			try {
+
+				// 出力ファイルの作成
+				FileWriter f = new FileWriter("C:\\sample\\kintai.csv", false);
+				PrintWriter p = new PrintWriter(new BufferedWriter(f));
+
+				// 内容をセットする
+				for(int i = 0; i < workingTimeList.size(); i++){
+					p.print(workingTimeList.get(i).getUserId());
+					p.print(",");
+					p.print(workingTimeList.get(i).getDate());
+					p.print(",");
+					p.print(workingTimeList.get(i).getYear());
+					p.print(",");
+					p.print(workingTimeList.get(i).getMonth());
+					p.print(",");
+					p.print(workingTimeList.get(i).getDay());
+					p.print(",");
+					p.print(workingTimeList.get(i).getWeekDay());
+					p.print(",");
+					p.print(workingTimeList.get(i).getBreakTime());
+					p.print(",");
+					p.print(workingTimeList.get(i).getOverTime());
+					p.print(",");
+					p.print(workingTimeList.get(i).getInTime());
+					p.print(",");
+					p.print(workingTimeList.get(i).getOutTime());
+					p.print(",");
+					p.print(workingTimeList.get(i).getWorkStartTime());
+					p.print(",");
+					p.print(workingTimeList.get(i).getWorkEndTime());
+					p.print(",");
+
+					p.println();    // 改行
+				}
+
+
+				// ファイルに書き出し閉じる
+				p.close();
+
+				System.out.println("ファイル出力完了！");
+
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+
+		//		model.addAttribute("userId", userId);
+		//		model.addAttribute("year", workingTimeList.get(0).getYear() + "年");
+		//		model.addAttribute("month", workingTimeList.get(0).getMonth() + "月");
+		//        model.addAttribute("workingTimeList", workingTimeList);
+		return "workingTimeTable";
+	}
+
+	@RequestMapping(value = "/csvInForm", method = RequestMethod.POST)
+	public String getInCsv(Model model) {
+
+		try {
+			File f = new File("C:\\sample\\kintai.csv");
+			BufferedReader br = new BufferedReader(new FileReader(f));
+
+			String line;
+
+			// 1行ずつCSVファイルを読み込む
+			while ((line = br.readLine()) != null) {
+				String[] data = line.split(",", 0); // 行をカンマ区切りで配列に変換
+//				WorkingTimeEntity workingTimeCsv = new WorkingTimeEntity();
+//				List<WorkingTimeEntity> workingTimeCsvList = new ArrayList<WorkingTimeEntity>();
+//
+//				workingTimeCsv.setUserId(data[0]);
+//				workingTimeCsv.setDate(data[1]);
+//				workingTimeCsv.setYear(data[2]);
+//				workingTimeCsv.setMonth(data[3]);
+//				workingTimeCsv.setDay(data[4]);
+//				workingTimeCsv.setWeekDay(data[5]);
+//				workingTimeCsv.setBreakTime(data[6]);
+//				workingTimeCsv.setOverTime(data[7]);
+//				workingTimeCsv.setInTime(data[8]);
+//				workingTimeCsv.setOutTime(data[9]);
+//				workingTimeCsv.setworkStartTime(data[10]);
+//				workingTimeCsv.setworkEndTime(data[11]);
+
+//				workingTimeCsvList.add(workingTimeCsv);
+
+				//	        	  workingTimeCsvList.get(i).setUserId(data[index]);
+				//	        	  workingTimeCsvList.get(i).setDate(data[++index]);
+				//	        	  workingTimeCsvList.get(i).setYear(data[++index]);
+				//	        	  workingTimeCsvList.get(i).setMonth(data[++index]);
+				//	        	  workingTimeCsvList.get(i).setDay(data[++index]);
+				//	        	  workingTimeCsvList.get(i).setWeekDay(data[++index]);
+				//	        	  workingTimeCsvList.get(i).setBreakTime(data[++index]);
+				//	        	  workingTimeCsvList.get(i).setOverTime(data[++index]);
+				//	        	  workingTimeCsvList.get(i).setInTime(data[++index]);
+				//	        	  workingTimeCsvList.get(i).setOutTime(data[++index]);
+				//	        	  workingTimeCsvList.get(i).setworkStartTime(data[++index]);
+				//	        	  workingTimeCsvList.get(i).setworkEndTime(data[++index]);
+
+				timeCardRepo.insertWorkingTime(
+						data[0],
+						data[1],
+						data[2],
+						data[3],
+						data[4],
+						data[5],
+						data[6],
+						data[7],
+						data[8],
+						data[9],
+						data[10],
+						data[11]);	
+			}
+			br.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+
+		//		model.addAttribute("userId", userId);
+		//		model.addAttribute("year", workingTimeList.get(0).getYear() + "年");
+		//		model.addAttribute("month", workingTimeList.get(0).getMonth() + "月");
+		//        model.addAttribute("workingTimeList", workingTimeList);
+		return "workingTimeTable";
+	}
 }
--- src/main/resources/static/js/workingTimeTable.js
+++ src/main/resources/static/js/workingTimeTable.js
@@ -1,31 +1,64 @@
 /**
- * 
+ * CSV出力
  */
+$(function() {
+  $('.btn-csv').click(function() {
+    var d = [];
+    $('table:eq(2) tr').each(function(i, e) {
+        var dd = [];
+        $(this).find('td').each(function(j, el) {
+          dd.push($(this).text());
+        })
+        d.push(dd);
+    })
+    // BOM の用意（文字化け対策）
+    var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
 
-/*
-$(function(){
-	$(".btn-date").on('click', function() {
-		if($(".btn-date").text() == "IN"){
-				$(".btn-in").text(nowTime);
-				let date = today + "," + nowTime;
-				$(".btn-in").val(date);
-//;
-			}
-	});
-	$(".btn-out").on('click', function() {
-		if($(".btn-out").text() == "OUT"){
-				$(".btn-out").text(nowTime);
-				let date = today + "," + nowTime;
-				$(".btn-out").val(date);
-			}
-	});
+    // CSV データの用意
+    var csv_data = d.map(function(l){return l.join(',')}).join('\r\n');
+    var blob = new Blob([bom, csv_data], { type: 'text/csv' });
+    var url = (window.URL || window.webkitURL).createObjectURL(blob);
+    var a = document.getElementById('downloader');
+    a.download = 'data.csv';
+    a.href = url;
+
+    // ダウンロードリンクをクリックする
+    $('#downloader')[0].click();
+  })
 })
-*/
-//$(function(){
-//	$(".btn-date").on('click', function() {
-//				let year = $(".list-year").val();
-//				$(".input-year").val(year);
-//				let month = $(".list-month").val();
-//				$(".input-month").val(month);
-//	});
+
+//$(function() {
+//  $('.btn-csv').click(function() {
+//    var d = [];
+//    var c = [];
+//    $('table tr').each(function(i, e) {
+//      var dd = [];
+//      var cc = [];
+//      if(i === 0) {
+//        $(this).find('th').each(function(j, el) {
+//          cc.push($(this).text());
+//        })
+//        c.push(cc)
+//      } else {
+//        $(this).find('td').each(function(j, el) {
+//          dd.push($(this).text());
+//        })
+//        d.push(dd);
+//      }
+//    })
+//    var m = $.merge(c, d);
+//    // BOM の用意（文字化け対策）
+//    var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
+//
+//    // CSV データの用意
+//    var csv_data = m.map(function(l){return l.join(',')}).join('\r\n');
+//    var blob = new Blob([bom, csv_data], { type: 'text/csv' });
+//    var url = (window.URL || window.webkitURL).createObjectURL(blob);
+//    var a = document.getElementById('downloader');
+//    a.download = 'data.csv';
+//    a.href = url;
+//
+//    // ダウンロードリンクをクリックする
+//    $('#downloader')[0].click();
+//  })
 //})
